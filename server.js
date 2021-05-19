bab986de5e4fdfe17bfada6916c32afd0550a2a2
Ryan Yeske <ryan@ryanyeske.com>
2021-05-18 22:35:57 -0700

    better error handling on liquidsoap socket connection

--- server.js
+++ server.js
@@ -44,17 +44,22 @@ async function liquidsoap(command, host = '127.0.0.1', port = 1234) {
   return new Promise((resolve, reject) => {
     client.connect(port, host, function() {
       client.write(command + '\n');
-
-      client.on('data', function(data) {
-        resolve(data)
-        client.write('quit\n')
-        client.destroy();
-      });
-
-      client.on('close', function() {
-        console.log('liquidsoap telnet connection closed');
-      });
     })
+
+    client.on('data', function(data) {
+      resolve(data.toString())
+      client.write('quit\n')
+      client.destroy();
+    });
+
+    client.on('close', function() {
+      console.log('liquidsoap telnet connection closed');
+    });
+
+    client.on('error', function(e) {
+      console.error(e)
+      reject(e)
+    });
   })
 }
 
