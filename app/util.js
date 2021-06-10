6365caa58d8228441fe951f49799d52a17ed227e
Ryan Yeske <ryan@ryanyeske.com>
2021-06-09 23:39:07 -0700

    remove leading zero on minutes

--- app/util.js
+++ app/util.js
@@ -1,7 +1,7 @@
 function formatDuration(s) {
   const minutes = Math.floor(s / 60)
   const seconds = s % 60
-  return `0${minutes}`.slice(-2) + ':' + `0${seconds}`.slice(-2)
+  return `${minutes}` + ':' + `0${seconds}`.slice(-2)
 }
 
 module.exports = {
