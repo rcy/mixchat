0dd160bbaa8a320dbc1acb0d5a5ceef01045c73c
Ryan Yeske <ryan@ryanyeske.com>
2021-08-24 16:40:57 -0700

    dispatch to handlers, add echo command

--- worker/tasks/event_created.js
+++ worker/tasks/event_created.js
@@ -12,31 +12,11 @@ module.exports = async ({ id }, helpers) => {
     const command = args.shift().toLowerCase()
 
     // ================ dispatch to handlers here
-    if (command === 'add') {
-      await insertResult({ status: 'adding' })
-
-      const url = args.join(' ')
-      console.log(`ripping ${url}`)
-
-      let filename;
-
-      // download
-      try {
-        filename = await youtubeDownload(url)
-        await insertResult({ status: 'added', filename })
-      } catch(e) {
-        console.error(e)
-        await insertResult({ status: 'error', error: e.message })
-      }
-
-      // add track to db
-      try {
-        const { rows } = await helpers.query("insert into tracks (filename, event_id) values ($1::text, $2::integer) returning id", [filename, event.id]);
-        await insertResult({ status: 'queued', filename, id: rows[0].id })
-      } catch(e) {
-        console.error(e)
-        await insertResult({ status: 'error', message: e.message, code: e.code, detail: e.detail })
-      }
+    const handler = handlers[command]
+    if (handler) {
+      handler(args, { event, insertResult, helpers })
+    } else {
+      insertResult({ error: `no such command: ${command}` })
     }
   }
 
@@ -44,3 +24,36 @@ module.exports = async ({ id }, helpers) => {
     return await helpers.query("insert into results (event_id, name, data) values ($1::integer, $2::text, $3::jsonb) returning id", [event.id, 'IRC_RESPONSE', data])
   }
 };
+
+const handlers = {
+  echo: async function(args, { insertResult }) {
+    insertResult({ msg: args })
+  },
+  add: async function(args, { event, helpers, insertResult }) {
+    await insertResult({ status: 'adding' })
+
+    const url = args.join(' ')
+    console.log(`ripping ${url}`)
+
+    let filename;
+
+    // download
+    try {
+      filename = await youtubeDownload(url)
+      await insertResult({ status: 'added', filename })
+    } catch(e) {
+      console.error(e)
+      await insertResult({ status: 'error', error: e })
+      return
+    }
+
+    // add track to db
+    try {
+      const { rows } = await helpers.query("insert into tracks (filename, event_id) values ($1::text, $2::integer) returning id", [filename, event.id]);
+      await insertResult({ status: 'queued', filename, id: rows[0].id })
+    } catch(e) {
+      console.error(e)
+      await insertResult({ status: 'error', message: e.message, code: e.code, detail: e.detail })
+    }
+  }
+}
