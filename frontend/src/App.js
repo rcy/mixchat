775de1e700bce5194ec7d35123b62cf394db10f3
Ryan Yeske <ryan@ryanyeske.com>
2022-01-15 09:13:50 -0700

    two station support, rough

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,23 @@
 import logo from './banana.png';
-//import './App.css';
-import { useQuery, gql } from '@apollo/client';
-import { useEffect, useState } from 'react';
+import './App.css';
 import { Outlet, Link, Routes, Route } from "react-router-dom";
 import StationPage from './StationPage.js';
-
-function CurrentTrack({ station }) {
-  const [track, setTrack] = useState()
-
-  const { loading, error, data } = useQuery(gql`
-    query RecentlyPlayed {
-      allTrackEvents(condition: { stationId: 1, action: "played"}, orderBy: CREATED_AT_DESC, first: 5) {
-        edges {
-          node {
-            createdAt
-            stationId
-            id
-            action
-            trackByTrackId {
-              id
-              filename
-            }
-          }
-        }
-      }
-    }
-  `, {
-    pollInterval: 10000,
-  });
-
-  useEffect(() => {
-    if (!loading) {
-      setTrack(data.allTrackEvents.edges[0].node.trackByTrackId)
-    }
-  }, [data]);
-
-  return <p>{track?.filename.replace(/^\/media\//,'').replace(/\.ogg$/,'')}</p>
-}
 
 function App() {
   return (
     <div className="App">
-      <Link to="/emb">emb</Link> | <Link to="/tlm">tlm</Link>
-      <Outlet />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <h1>
           DJFULLMOON
         </h1>
-        <p>
-          <a
-            className="App-link"
-            href="https://web.libera.chat/#djfullmoon"
-            target="_blank"
-            rel="noopener noreferrer"
-          >join the chat</a>
-        </p>
-        <Routes>
-          <Route path=":slug" element={<StationPage />} />
-        </Routes>
-
-        <CurrentTrack station="emb" />
       </header>
+
+      <Routes>
+        <Route path=":slug" element={<StationPage />} />
+      </Routes>
+
+      <Link to="/emb">emb</Link> | <Link to="/tlm">tlm</Link>
+
     </div>
   );
 }
--- frontend/src/StationPage.js
+++ frontend/src/StationPage.js
@@ -1,18 +1,94 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from '@apollo/client';
+
+function AudioControl({ stationSlug }) {
+  const audioRef = useRef(null)
+
+  useEffect(() => {
+    // <source dataFormat="ogg" src={`https://stream.djfullmoon.com/${slug}.ogg`} type="audio/ogg" />
+    // <source dataFormat="mp3" src={`https://stream.djfullmoon.com/${slug}.mp3`} type="audio/mp3" />
+    audioRef.current.src = `https://stream.djfullmoon.com/${stationSlug}.ogg`
+  }, [stationSlug])
+
+  return (
+    <audio controls ref={audioRef} autoPlay />
+  )
+}
+
+function CurrentTrack({ stationId }) {
+  const [track, setTrack] = useState()
+
+  const { loading, error, data } = useQuery(gql`
+    query RecentlyPlayed($stationId: Int!) {
+      allTrackEvents(condition: { stationId: $stationId, action: "played"}, orderBy: CREATED_AT_DESC, first: 5) {
+        edges {
+          node {
+            createdAt
+            stationId
+            id
+            action
+            trackByTrackId {
+              id
+              filename
+            }
+          }
+        }
+      }
+    }
+  `, {
+    pollInterval: 10000,
+    variables: { stationId }
+  });
+
+  useEffect(() => {
+    if (!loading && data) {
+      setTrack(data.allTrackEvents.edges[0].node.trackByTrackId)
+    }
+  }, [stationId, data, loading]);
+
+  return <p>{track?.filename.replace(/^\/media\//,'').replace(/\.ogg$/,'')}</p>
+}
+
 
 function StationPage() {
   const params = useParams()
 
-  useEffect(() => {
-    console.log('station changed', params.slug)
-  }, [params])
+  const { loading, error, data } = useQuery(gql`
+    query StationBySlug($slug: String!) {
+      stationBySlug(slug: $slug) {
+        id
+        slug
+        ircChannelByStationId {
+          id
+          channel
+        }
+      }
+    }`, { variables: { slug: params.slug } });
+
+  if (loading) {
+    return "spinner"
+  }
   
+  const station = data.stationBySlug
+
+  const channel = station?.ircChannelByStationId?.channel
+
   return (
-    <audio controls>
-      <source src={`https://stream.djfullmoon.com/${params.slug}.ogg`} type="audio/ogg" />
-      <source src={`https://stream.djfullmoon.com/${params.slug}.mp3`} type="audio/mp3" />
-    </audio>
+    <div>
+      <h2>{station.slug}</h2>
+      <p>
+        <a
+          className="App-link"
+          href={`https://web.libera.chat/${channel}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >join the {channel} chat</a>
+      </p>
+
+      <AudioControl stationSlug={station.slug} />
+      <CurrentTrack stationId={station.id} />
+    </div>
   )
 }
 
