f9e1cfbd6263a43bbf7bdcca15fbcf9dea0199d0
Ryan Yeske <ryan@ryanyeske.com>
2022-01-17 07:42:17 -0700

    frontend: remove hardcoded graphql and icecast url endpoints

--- frontend/src/AudioControl.js
+++ frontend/src/AudioControl.js
@@ -6,11 +6,12 @@ export default function AudioControl({ stationSlug }) {
   useEffect(() => {
     // <source dataFormat="ogg" src={`https://stream.djfullmoon.com/${slug}.ogg`} type="audio/ogg" />
     // <source dataFormat="mp3" src={`https://stream.djfullmoon.com/${slug}.mp3`} type="audio/mp3" />
-    audioRef.current.src = `https://stream.djfullmoon.com/${stationSlug}.ogg`
+    const streamUrl = `${process.env.REACT_APP_ICECAST_URL}/${stationSlug}.ogg`
+    audioRef.current.src = streamUrl
+    console.log({ streamUrl })
   }, [stationSlug])
 
   return (
     <audio controls ref={audioRef} autoPlay />
   )
 }
-
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://api.djfullmoon.com/graphql',
+  uri: process.env.REACT_APP_GRAPHQL_URL,
   cache: new InMemoryCache(),
 });
 
