c92e1eab8325a5f407a3ab59ec60c8b460f4e8ef
Ryan Yeske <ryan@ryanyeske.com>
2022-01-22 06:22:57 -0700

    fix some nagging pg and ls bugs

--- app/webserver.js
+++ app/webserver.js
@@ -37,35 +37,49 @@ module.exports = function webserver({ pgClient, port }) {
   })
 
   app.get('/next/:station_slug', async (req, res) => {
-    const { rows: [{ station_id }] } = await pgClient.query("select id as station_id from stations where slug = $1", [req.params.station_slug]);
+    try {
+      const { rows: [{ station_id }] } = await pgClient.query("select id as station_id from stations where slug = $1", [req.params.station_slug]);
 
-    const { rows: [track] } = await pgClient.query('select id, filename from tracks where station_id = $1 order by bucket, fuzz, created_at limit 1', [station_id])
+      const { rows: [track] } = await pgClient.query('select id, filename from tracks where station_id = $1 order by bucket, fuzz, created_at limit 1', [station_id])
 
-    if (!track) {
-      console.error(`${req.params.station_slug}: no track found, sending ./404.ogg`)
-      res.status(404).send('./404.ogg')
-      return
-    } else {
-      console.log(`/next/${req.params.station_slug}`, { track, station_id })
-      await pgClient.query('insert into track_events (station_id, track_id, action) values ($1, $2, $3)', [station_id, track.id, 'queued'])
-      res.send(`${track.filename}\n`)
+      if (!track) {
+        console.error(`${req.params.station_slug}: no track found`)
+        res.status(404).send('404')
+        return
+      } else {
+        console.log(`/next/${req.params.station_slug}`, { track, station_id })
+        await pgClient.query('insert into track_events (station_id, track_id, action) values ($1, $2, $3)', [station_id, track.id, 'queued'])
+        res.send(`${track.filename}`)
+      }
+    } catch(e) {
+      console.error(e)
+      res.status(500).send('500')
     }
   })
 
-  app.post('/now/:station_slug', jsonParser, async (req, res) => {
+  app.post('/now/:slug', jsonParser, async (req, res) => {
     console.log('SENT NOW', req.params, req.body.filename)
 
+    const { slug } = req.params;
+    const { filename } = req.body;
+
     try {
-      await pgClient.query(`
-insert into track_events (
-   station_id,
-   track_id,
-   action
-) values (
-   (select id from stations where slug = $1),
-   (select id from tracks where filename = $2),
-   'played'
-)`, [req.params.station_slug, req.body.filename]);
+      const { rows } = await pgClient.query(`
+select tracks.id as track_id, stations.id as station_id
+ from tracks 
+ join stations on tracks.station_id = stations.id
+ where tracks.filename = $1 and stations.slug = $2
+      `, [filename, slug])
+
+      if (rows[0]) {
+        await pgClient.query(`
+  insert into track_events (
+     station_id,
+     track_id,
+     action
+  ) values ($1, $2, 'played')
+        `, [rows[0].station_id, rows[0].track_id]);
+      }
     } catch(e) {
       // TODO: handle better
       console.error(e);
--- db/migrations/current.sql
+++ db/migrations/current.sql
@@ -1,1 +1,7 @@
 -- Enter migration here
+
+-- there were two unique filename constraints
+alter table tracks drop constraint if exists tracks_filename_key;
+alter table tracks drop constraint if exists tracks_unique_filename;
+
+alter table tracks add constraint tracks_unique_station_filename unique (station_id, filename);
