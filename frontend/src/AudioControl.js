02d562a0d3fae7285bf024a751a8530e45b4563f
Ryan Yeske <ryan@ryanyeske.com>
2022-01-24 22:10:57 -0700

    frontend: add mp3 source

--- frontend/src/AudioControl.js
+++ frontend/src/AudioControl.js
@@ -2,16 +2,20 @@ import { useEffect, useRef } from 'react';
 
 export default function AudioControl({ stationSlug }) {
   const audioRef = useRef(null)
+  const oggSourceRef = useRef(null)
+  const mp3SourceRef = useRef(null)
 
   useEffect(() => {
-    // <source dataFormat="ogg" src={`https://stream.djfullmoon.com/${slug}.ogg`} type="audio/ogg" />
-    // <source dataFormat="mp3" src={`https://stream.djfullmoon.com/${slug}.mp3`} type="audio/mp3" />
-    const streamUrl = `${process.env.REACT_APP_ICECAST_URL}/${stationSlug}.ogg`
-    audioRef.current.src = streamUrl
-    console.log({ streamUrl })
+    const oggStreamUrl = `${process.env.REACT_APP_ICECAST_URL}/${stationSlug}.ogg`
+    const mp3StreamUrl = `${process.env.REACT_APP_ICECAST_URL}/${stationSlug}.mp3`
+    oggSourceRef.current.src = oggStreamUrl
+    mp3SourceRef.current.src = mp3StreamUrl
   }, [stationSlug])
 
   return (
-    <audio controls ref={audioRef} autoPlay />
+    <audio controls ref={audioRef} autoPlay>
+      <source ref={oggSourceRef} type="audio/mp3" />
+      <source ref={mp3SourceRef} type="audio/ogg" />
+    </audio>
   )
 }
