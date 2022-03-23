55cad68dc59a8d7b9f4348a015252325d3be9d7c
Ryan Yeske <ryan@ryanyeske.com>
2022-03-22 23:02:50 -0700

    try no whitespace prefix

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -225,7 +225,7 @@ function hashString(s, max) {
 
 function Nick({ message, prevMessage }) {
   if (message.nick === prevMessage?.nick) {
-    return <span>&zwj;</span>;
+    return null;//<span>&zwj;</span>;
   }
   const color = colors[hashString(message.nick, colors.length)]
   return (<span style={{ color }}><b>{message.nick}</b></span>)
@@ -243,7 +243,7 @@ function Time({ message, prevMessage }) {
 
   const show = newNick //|| newTime
 
-  return <span
+  return show && <span
            style={{
              opacity: '50%',
              //visibility: mt === pt ? 'hidden' : 'visible',
