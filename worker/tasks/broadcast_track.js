78eaa3a7f241950773b04fafa50f3df1e4cc1513
Ryan Yeske <ryan@ryanyeske.com>
2022-12-28 01:17:18 -0700

    Log error if env var not set

--- worker/tasks/broadcast_track.js
+++ worker/tasks/broadcast_track.js
@@ -13,6 +13,10 @@ module.exports = async ({ id }, helpers) => {
 
   console.log({ rows, artist, title, purl })
 
+  if (!process.env.FLAPPER_URL) {
+    throw new Error('FLAPPER_URL not set');
+  }
+
   const result = await fetch(process.env.FLAPPER_URL, {
     method: "POST",
     body: `${artist}, ${title} ${purl}`
