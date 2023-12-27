eddca9ee6fb3fec35cf952ea974ca744d4deacbf
Ryan Yeske <ryan@ryanyeske.com>
2023-12-27 15:49:24 -0700

    Show listening gap port

--- gap/cmd/api/main.go
+++ gap/cmd/api/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-
 	server := server.NewServer()
 
+	fmt.Println("listening on", server.Addr)
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
