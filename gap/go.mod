55a17add3920004d08f845a3228ca8ed949609f7
Ryan Yeske <ryan@ryanyeske.com>
2024-01-05 19:10:06 -0700

    Go version 1.21

--- gap/go.mod
+++ gap/go.mod
@@ -1,6 +1,6 @@
 module gap
 
-go 1.20
+go 1.21.5
 
 require (
 	github.com/dhowden/tag v0.0.0-20230630033851-978a0926ee25
--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
+let
+  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+in
+{ nixpkgs ? import <nixpkgs> {} }:
+with nixpkgs; mkShell {
   buildInputs = with pkgs; [
     air
-    go
-    gopls
+    unstable.go_1_21
+    unstable.gopls
     nodejs
     foreman
     liquidsoap
