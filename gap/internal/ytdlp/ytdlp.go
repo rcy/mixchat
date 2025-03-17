2f3078781aedfabc968d716a680cbc1ef8390cb3
Ryan Yeske <ryan@ryanyeske.com>
2025-03-16 18:07:53 -0700

    Reduce number of youtube search results returned
    API credit usage is based on results returned

--- gap/internal/ytdlp/ytdlp.go
+++ gap/internal/ytdlp/ytdlp.go
@@ -106,8 +106,9 @@ func Search(ctx context.Context, query string) ([]Result, error) {
 
 	// Step 1: Search for videos
 	searchCall := youtubeService.Search.List([]string{"id", "snippet"}).
-		Q(query).       // Search query
-		MaxResults(128) // Number of results to retrieve
+		Q(query).
+		MaxResults(10)
+		//SafeSearch("strict")
 
 	searchResponse, err := searchCall.Do()
 	if err != nil {
